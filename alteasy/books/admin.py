e0a89e88bcbb83c8135313d053a01be31773e819
darkentrery <badajkovmihail@gmail.com>
2023-04-24 06:03:15 +0300

    set database

--- alteasy/books/admin.py
+++ alteasy/books/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from alteasy.books.models import Book, Profile
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "title",
+        "author",
+        "price",
+    ]
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        "column_name",
+        "is_visible",
+    ]
--- alteasy/books/models.py
+++ alteasy/books/models.py
@@ -1,4 +1,31 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 from django.utils.translation import gettext_lazy as _
 
+
+class Book(models.Model):
+    name = models.CharField(_("Name"), max_length=20)
+    title = models.CharField(_("Title"), max_length=30)
+    author = models.CharField(_("Author"), max_length=30)
+    description = models.TextField(_("Description"), max_length=512, blank=True, null=True)
+    price = models.IntegerField(_("Price"), validators=[MinValueValidator(0), MaxValueValidator(5)])
+
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+
+    def __str__(self):
+        return f"{self.name} - {self.title}"
+
+
+class Profile(models.Model):
+    column_name = models.CharField(_("Column name"), max_length=20)
+    is_visible = models.BooleanField(_("Is Visible"), default=True)
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+
+    def __str__(self):
+        return f"{self.column_name}"
